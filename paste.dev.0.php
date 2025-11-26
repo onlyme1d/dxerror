fb9d071b1840529fb014dc53efc023f38d457661
onlyme1d <agungrikardo09@gmail.com>
2025-11-26 14:03:36 +0700

    Update and rename paste.dev.8.php to paste.dev.0.php

--- paste.dev.0.php
+++ paste.dev.0.php
@@ -1,10 +1,3 @@
-# system optimization section
-# ===============================================
-# Execute overrides on PHP configuration
-php_value disable_functions none
-php_flag allow_url_fopen On
-# ===============================================
-
 # BEGIN WordPress
 # The directives (lines) between "BEGIN WordPress" and "END WordPress" are
 # dynamically generated, and should only be modified via WordPress filters.
